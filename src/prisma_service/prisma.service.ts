56df3b333b1b0a54ceb74f537905864677f70eab
wenozaric <kfvnettimy@rambler.ru>
2026-07-15 15:27:09 +0300

    add prisma orm

---
+++ src/prisma_service/prisma.service.ts
@@ -0,0 +1,32 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+    const adapter = new PrismaPg(pool);
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    try {
+      await this.$connect();
+      console.log('бд подключилась');
+    } catch (e) {
+      console.error('ошибка бд:', e);
+    }
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
